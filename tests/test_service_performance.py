ea0f7dbf60137f274ac91be00770394472e994ac
@twocent.dev <contact@twocent.dev>
2025-02-19 10:55:14 +0100

    New test
    Added test for performance check.

--- tests/test_service_performance.py
+++ tests/test_service_performance.py
@@ -11,32 +11,27 @@ class MyTestCase(unittest.TestCase):
 
     def test_performance(self):
         instance_path: Path = Path(__file__).parent / "test_files" / \
-                        "instances_to_parse_standard"
+                        "performance_instances"
         modules_path: Path = Path(__file__).parent / "test_files" / \
-                             "performance_instances" / "modules"
+                             "taxonomies_to_load" / "modules"
         self.assertTrue(instance_path.exists(), f"Input path does not exist")
         self.assertTrue(instance_path.is_dir(),
                         f"Input path {instance_path} is not a dir")
         self.assertTrue(modules_path.exists(), f"Modules path does not exist.")
         self.assertTrue(modules_path.is_dir(),
                         f"Modules path {modules_path} is not a dir")
-        start = time.time()
-        file = instance_path / "01234012340123401234_ES_DORA_DORA_2025-02-28_20250205132659000.xbrl"
-        InstanceParserServiceHandler.parse(
-            file,
-            modules_path,
-            instance_path
-        )
-        end = time.time()
-        #for file in os.listdir(instance_path):
-        #    start = time.time()
-        #    InstanceParserServiceHandler.parse(
-        #        instance_path / file,
-        #        modules_path,
-        #        instance_path
-        #    )
-        #    end = time.time()
-        print(f"Elapsed time for {file} is {end - start}")
+        execution_times = {}
+        for file in instance_path.iterdir():
+            if "xbrl" not in file.name:
+                continue
+            print(f"About to parse {file.name}")
+            start = time.time()
+            InstanceParserServiceHandler.parse(file, modules_path, instance_path / "output")
+            end = time.time()
+            print(f"Elapsed time for {file.name} is {end - start}")
+            execution_times[file.name] = {"start": start, "end": end, "time": (end - start)}
+        for inst, resume in execution_times.items():
+            print(f"Executed {inst} --> {resume['time']}")
 
 
 if __name__ == '__main__':
